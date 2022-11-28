32faf1d2a1fe8c371497a4fcb273b091580711d9
severus <frayseverus@gmail.com>
2022-11-28 12:11:52 -0300

    Escribiendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//los siguientes son ordenes que solo funcionan en el explorador como entorno de ejecucion.
 const h1 = document.querySelector('h1');
 const primerParrafo = document.querySelector('.parrafinio');
 const segundoParrafo = document.querySelector('#pid');
@@ -13,18 +12,42 @@ console.log({
     input
 });
 
-/*
-constante nombreDeConstante = document.querySelector('etiqueta');
-constante nombreDeConstante = document.querySelector('.clase');
-constante nombreDeConstante = document.querySelector('#id');
+h1.innerHTML = '<i>Subele la radio que esta es mi cancion</i>';
+// inserta codigo html
 
-constante nombreDeConstante = document...
-    ....querySelector('etiqueta') para el primer caso de dicha etiqueta
-    ....querySelectorAll('etiqueta') para seleccionar todos los casos
-    ....getElementById('#id') Hace lo mismo pero solo funciona con ID. Asume individualidad
-    ....getElementsByClass('.etiqueta') Hace lo mismo pero solo funciona con clases. Asume pluralidad
+primerParrafo.innerText = 'Esta etiqueta <b> no deberia verse pero se ve porque es un .innerText <b> y no un .innerHTML';
+//inserta texto
 
-const input = document.querySelector('input');
-    console.log(input.value); //devuelve el valor de la propiedad value
-    console.log(input.hidden), //devuelve el valor de la propiedad hidden, el cual seguramente sea f
+console.log(h1.getAttribute('holderplace'));
+// getAttribute devuelve el valor del atributo, igual que poner h1.holderplace
+
+input.setAttribute('class', 'claseInput');
+//define un atributo y lo asigna
+
+segundoParrafo.classList.add("superClase");
+//Metodo especifico de clases. Submetodo para agregar una nueva clase.
+
+primerParrafo.classList.remove("parrafinio");
+//Metodo especifico de clases. Submetodo para quitar una nueva clase.
+
+input.id = "npt_generico";
+//Esta es otra forma de acceder al seteo de propiedades. Se trata como un objeto de hecho. input.id devuelve el id si hiciera falta
+
+const img = document.createElement('img');
+img.src = ('./imagen.png');
+img.classList.add("img");
+segundoParrafo.append(img);
+/* 
+La primer linea sirve para crear un elemento html. La etiqueta sera img
+El segundo determina la funete del source
+La tercer linea (vista mas arriba) agregale una clase
+El ultimo codigo inserta a img en el segundoParrafo
+*/
+
+
+
+/* queda pendiente 
+    .appendChild()
+    .classList.toggle(...)
+    .classList.contains(...) que devuelve true o false depende si contiene o no algo.
 */
