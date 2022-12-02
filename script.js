6a8c51113b70b493c0f1cf9ff9cab77642fbed34
severus <frayseverus@gmail.com>
2022-12-01 22:10:00 -0300

    Eventos en JavaScript: interactuando con usuarios

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-const h1 = document.querySelector('h1');
-const primerParrafo = document.querySelector('.parrafinio');
-const segundoParrafo = document.querySelector('#pid');
-const input = document.querySelector('input');
+const npt_calculo1 = document.querySelector('#npt_calculo1');
+const npt_calculo2 = document.querySelector('#npt_calculo2');
 
-console.log(input.value);
+function sumar() {
+    return resultado =
+        Number(npt_calculo1.value) +
+        Number(npt_calculo2.value);
+};
 
-console.log({
-    h1,
-    primerParrafo,
-    segundoParrafo,
-    input
-});
+const randomName = document.querySelector("body");
 
-h1.innerHTML = '<i>Subele la radio que esta es mi cancion</i>';
-// inserta codigo html
-
-primerParrafo.innerText = 'Esta etiqueta <b> no deberia verse pero se ve porque es un .innerText <b> y no un .innerHTML';
-//inserta texto
-
-console.log(h1.getAttribute('holderplace'));
-// getAttribute devuelve el valor del atributo, igual que poner h1.holderplace
-
-input.setAttribute('class', 'claseInput');
-//define un atributo y lo asigna
-
-segundoParrafo.classList.add("superClase");
-//Metodo especifico de clases. Submetodo para agregar una nueva clase.
-
-primerParrafo.classList.remove("parrafinio");
-//Metodo especifico de clases. Submetodo para quitar una nueva clase.
-
-input.id = "npt_generico";
-//Esta es otra forma de acceder al seteo de propiedades. Se trata como un objeto de hecho. input.id devuelve el id si hiciera falta
-
-const img = document.createElement('img');
-img.src = ('./imagen.png');
-img.classList.add("img");
-segundoParrafo.append(img);
-/* 
-La primer linea sirve para crear un elemento html. La etiqueta sera img
-El segundo determina la funete del source
-La tercer linea (vista mas arriba) agregale una clase
-El ultimo codigo inserta a img en el segundoParrafo
-*/
-
-
-
-/* queda pendiente 
-    .appendChild()
-    .classList.toggle(...)
-    .classList.contains(...) que devuelve true o false depende si contiene o no algo.
-*/
+randomName.innerHTML += `<p>Mi mama me mima mucho</p>`;
